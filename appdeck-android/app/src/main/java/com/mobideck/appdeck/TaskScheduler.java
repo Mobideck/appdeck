41755d8a3cdad3b1de3366d6b3be3cdc3a90e8ec
mobideck <mathieu@mobideck.net>
2015-09-14 16:01:27 +0200

    add task scheduler

---
+++ appdeck-android/app/src/main/java/com/mobideck/appdeck/TaskScheduler.java
@@ -0,0 +1,38 @@
+package com.mobideck.appdeck;
+
+import android.os.Handler;
+import android.util.ArrayMap;
+
+/**
+ * Created by Ofek on 19/08/2015.
+ */
+public class TaskScheduler extends Handler {
+    private ArrayMap<Runnable,Runnable> tasks = new ArrayMap<>();
+    public void scheduleAtFixedRate(final Runnable task,long delay,final long period) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                task.run();
+                postDelayed(this, period);
+            }
+        };
+        tasks.put(task, runnable);
+        postDelayed(runnable, delay);
+    }
+    public void scheduleAtFixedRate(final Runnable task,final long period) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                task.run();
+                postDelayed(this, period);
+            }
+        };
+        tasks.put(task, runnable);
+        runnable.run();
+    }
+    public void stop(Runnable task) {
+        Runnable removed = tasks.remove(task);
+        if (removed!=null) removeCallbacks(removed);
+    }
+
+}
