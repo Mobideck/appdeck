9e7dbe69d67087f52c287434253542b47f07f029
mobideck <mathieu@mobideck.net>
2015-12-21 10:56:54 +0100

    cleanup

--- appdeck-android/app/src/main/java/com/mobideck/appdeck/AppDeckApplication.java
+++ appdeck-android/app/src/main/java/com/mobideck/appdeck/AppDeckApplication.java
@@ -35,6 +35,7 @@ public class AppDeckApplication extends android.support.multidex.MultiDexApplica
 	@Override
 	public void onCreate()
 	{
+		//android.os.Debug.startMethodTracing("start");
 /*	     if (true) {
 	         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
 	                 .detectDiskReads()
@@ -50,63 +51,6 @@ public class AppDeckApplication extends android.support.multidex.MultiDexApplica
 	                 .build());
 	     }*/		
 		super.onCreate();
-        //Initialize TestFlight with your app token.
-        //TestFlight.takeOff(this, "d1574e34-b01a-44b7-b44f-231bad62036d");
-		//dexTool();
-
 	}		
 
-	
-	 /**
-     * Copy the following code and call dexTool() after super.onCreate() in
-     * Application.onCreate()
-     * <p>
-     * This method hacks the default PathClassLoader and load the secondary dex
-     * file as it's parent.
-     */
-     /*
-    @SuppressLint("NewApi")
-    private void dexTool() {
-
-        File dexDir = new File(getFilesDir(), "dlibs");
-        dexDir.mkdir();
-        File dexFile = new File(dexDir, "libs.apk");
-        File dexOpt = new File(dexDir, "opt");
-        dexOpt.mkdir();
-        try {
-            InputStream ins = getAssets().open("libs.apk");
-            if (dexFile.length() != ins.available()) {
-                FileOutputStream fos = new FileOutputStream(dexFile);
-                byte[] buf = new byte[4096];
-                int l;
-                while ((l = ins.read(buf)) != -1) {
-                    fos.write(buf, 0, l);
-                }
-                fos.close();
-            }
-            ins.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        ClassLoader cl = getClassLoader();
-        ApplicationInfo ai = getApplicationInfo();
-        String nativeLibraryDir = null;
-        if (Build.VERSION.SDK_INT > 8) {
-            nativeLibraryDir = ai.nativeLibraryDir;
-        } else {
-            nativeLibraryDir = "/data/data/" + ai.packageName + "/lib/";
-        }
-        DexClassLoader dcl = new DexClassLoader(dexFile.getAbsolutePath(),
-                dexOpt.getAbsolutePath(), nativeLibraryDir, cl.getParent());
-
-        try {
-            Field f = ClassLoader.class.getDeclaredField("parent");
-            f.setAccessible(true);
-            f.set(cl, dcl);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }	*/
-	
 }
