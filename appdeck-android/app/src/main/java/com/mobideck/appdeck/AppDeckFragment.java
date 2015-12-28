adf8fdd96189dac45e163a3b7f7cb7b66cbe6dc3
mobideck <mathieu@mobideck.net>
2015-12-28 10:41:28 +0100

    first try better image downloading

--- appdeck-android/app/src/main/java/com/mobideck/appdeck/AppDeckFragment.java
+++ appdeck-android/app/src/main/java/com/mobideck/appdeck/AppDeckFragment.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -19,10 +20,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class AppDeckFragment extends Fragment {
 
@@ -157,24 +161,82 @@ public class AppDeckFragment extends Fragment {
 			actionBarLogoUrl = screenConfiguration.logo;
 		if (actionBarLogoUrl != null)
 		{
-			Utils.downloadImage(actionBarLogoUrl, appDeck.actionBarHeight, new SimpleImageLoadingListener() {
+			//if (loader.actionBarContent == actionBarLogoUrl)
+			//	return;
+			/*
+			Picasso.with(loader)
+					.load(actionBarLogoUrl)
+					.resize(0, appDeck.actionBarHeight)
+					.into(new Target()
+					{
+						@Override
+						public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from)
+						{
+							Drawable d = new BitmapDrawable(loader.getResources(), bitmap);
+							ActionBar actionBar = loader.getSupportActionBar();
+							if (actionBar == null)
+								return;
+							actionBar.setTitle(null);
+							actionBar.setIcon(d);
+							actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP|ActionBar.DISPLAY_SHOW_HOME);
+							//loader.actionBarContent = actionBarLogoUrl;
+						}
+
+						@Override
+						public void onBitmapFailed(Drawable errorDrawable)
+						{
+						}
+
+						@Override
+						public void onPrepareLoad(Drawable placeHolderDrawable)
+						{
+						}
+					});
+			if (true)
+				return;*/
+			Utils.downloadImage(actionBarLogoUrl, appDeck.actionBarWidth, appDeck.actionBarHeight, new SimpleImageLoadingListener() {
 				@Override
 				public void onLoadingComplete(String imageUri, View view, final Bitmap loadedImage) {
 					if (loader == null || imageUri == null || loadedImage == null)
 						return;
+					/*
+					ImageView imageView = new ImageView(loader);
+					//imageView.getLayoutParams().height = appDeck.actionBarHeight;
+					imageView.setImageBitmap(loadedImage);
+					imageView.setLayoutParams(
+							new ViewGroup.LayoutParams(
+									// or ViewGroup.LayoutParams.WRAP_CONTENT
+									ViewGroup.LayoutParams.MATCH_PARENT,
+									// or ViewGroup.LayoutParams.WRAP_CONTENT,
+									ViewGroup.LayoutParams.MATCH_PARENT ) );
+
+					ActionBar actionBar = loader.getSupportActionBar();
+					if (actionBar == null)
+						return;
+					actionBar.setTitle(null);
+					actionBar.setCustomView(imageView);
+					actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP|ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_SHOW_CUSTOM);
+					Log.i(TAG, "logo have been set in action bar");*/
+
+/*					BitmapDrawable draw = new BitmapDrawable(loader.getResources(), loadedImage);
+					ActionBar actionBar = loader.getSupportActionBar();
+					if (actionBar == null)
+						return;
+					actionBar.setTitle(null);
+					actionBar.setIcon(draw);
+					actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP|ActionBar.DISPLAY_SHOW_HOME);
+					Log.i(TAG, "logo have been set in action bar");*/
+
 					// run appdeck init in his own thread
 					new Thread(new Runnable() {
 						@Override
 						public void run() {
 							final BitmapDrawable draw = new BitmapDrawable(loader.getResources(), loadedImage);
+							//draw.setAntiAlias(true);
 							Handler mainHandler = new Handler(loader.getMainLooper());
 							Runnable myRunnable = new Runnable() {
 								@Override
 								public void run() {
-									/*AppCompatActivity sa = (AppCompatActivity)AppDeckFragment.this.getActivity();
-									if (sa == null)
-										return;
-									ActionBar ac = sa.getSupportActionBar();*/
                                     ActionBar actionBar = loader.getSupportActionBar();
 									if (actionBar == null)
 										return;
@@ -187,7 +249,7 @@ public class AppDeckFragment extends Fragment {
 							mainHandler.post(myRunnable);
 
 						}
-					}, "appdeckInit").start();
+					}, "topbaricon").start();
 				}
 
 				@Override
@@ -197,7 +259,7 @@ public class AppDeckFragment extends Fragment {
 
 				@Override
 				public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-					Log.i(TAG, "logo action bar onLoadingFailed");
+					Log.i(TAG, "logo action bar onLoadingFailed:"+failReason);
 				}
 
 				@Override
@@ -206,6 +268,9 @@ public class AppDeckFragment extends Fragment {
 				}
 			}, loader);
 		} else {
+			//if (loader.actionBarContent == actionBarTitle)
+			//	return;
+			//loader.actionBarContent = actionBarTitle;
             ActionBar actionBar = loader.getSupportActionBar();
             if (actionBar == null)
                 return;
