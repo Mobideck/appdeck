06092cfca5542021cf3e4727968a40d2acb30a2e
mobideck <mathieu@mobideck.net>
2016-01-02 16:09:12 +0100

    use local proxy for downloading images

--- appdeck-android/app/src/main/java/com/mobideck/appdeck/AppDeckBaseImageDownloader.java
+++ appdeck-android/app/src/main/java/com/mobideck/appdeck/AppDeckBaseImageDownloader.java
@@ -2,7 +2,14 @@ package com.mobideck.appdeck;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+
 import android.content.Context;
+import android.net.Uri;
+
 import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
 
 public class AppDeckBaseImageDownloader extends BaseImageDownloader {
@@ -17,9 +24,19 @@ public class AppDeckBaseImageDownloader extends BaseImageDownloader {
 	@Override
 	public InputStream getStream(String imageUri, Object extra) throws IOException {
 		// try from resources first
-		CacheManagerCachedResponse cachedResponse = AppDeck.getInstance().cache.getEmbedResponse(imageUri);
+/*		CacheManagerCachedResponse cachedResponse = AppDeck.getInstance().cache.getEmbedResponse(imageUri);
 		if (cachedResponse != null)
-			return cachedResponse.getStream();
+			return cachedResponse.getStream();*/
 		return super.getStream(imageUri, extra);
+	}
+
+	@Override
+	protected HttpURLConnection createConnection(String url, Object extra) throws IOException {
+		String encodedUrl = Uri.encode(url, ALLOWED_URI_CHARS);
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(AppDeck.getInstance().proxyHost, AppDeck.getInstance().proxyPort));
+		HttpURLConnection conn = (HttpURLConnection) new URL(encodedUrl).openConnection(proxy);
+		conn.setConnectTimeout(connectTimeout);
+		conn.setReadTimeout(readTimeout);
+		return conn;
 	}
 }
