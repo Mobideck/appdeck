e47dfeb5c570664bb905381037afd9a02dca6c99
mobideck <mathieu@mobideck.net>
2016-04-07 13:04:21 +0200

    update conf

--- appdeck-ios/AppDeck/AppDeck/AppDelegate.m
+++ appdeck-ios/AppDeck/AppDeck/AppDelegate.m
@@ -99,6 +99,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+    application.applicationIconBadgeNumber = 0;
     [FBSDKAppEvents activateApp];
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
