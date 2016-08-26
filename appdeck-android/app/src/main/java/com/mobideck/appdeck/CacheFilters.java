139337569d8e0e69d332222f4851e0533733a3f1
mobideck <mathieu@mobideck.net>
2016-08-26 15:30:21 +0200

    add close header

--- appdeck-android/app/src/main/java/com/mobideck/appdeck/CacheFilters.java
+++ appdeck-android/app/src/main/java/com/mobideck/appdeck/CacheFilters.java
@@ -1,28 +1,23 @@
 package com.mobideck.appdeck;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.zip.GZIPOutputStream;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.DefaultHttpContent;
-import io.netty.handler.codec.http.DefaultHttpRequest;
-import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.DefaultLastHttpContent;
 import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
@@ -32,13 +27,11 @@ import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.LastHttpContent;
 
 import org.apache.commons.io.IOUtils;
-//import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.littleshoot.proxy.HttpFilters;
 
 import android.util.Log;
-import android.webkit.WebResourceResponse;
 
 public class CacheFilters implements HttpFilters {
 
@@ -49,12 +42,12 @@ public class CacheFilters implements HttpFilters {
 	protected String absoluteURL;
 	
 	protected boolean isFirstRequest = false;
-	protected boolean shouldInjectAppDeckJS = false;
+	//protected boolean shouldInjectAppDeckJS = false;
 	protected boolean forceCache = false;
 	protected boolean forceReadFromCache = false;
-	protected boolean isInCache = false;
-	protected boolean shouldStoreInCache = false;
-	protected int shouldStoreInCacheTTL = 0;
+	//protected boolean isInCache = false;
+	//protected boolean shouldStoreInCache = false;
+	//protected int shouldStoreInCacheTTL = 0;
 
     protected boolean disableCache = false;
 
@@ -66,14 +59,14 @@ public class CacheFilters implements HttpFilters {
     
     
     protected OutputStream cacheStream = null;
-    protected OutputStream metaStream = null;
+    //protected OutputStream metaStream = null;
     protected boolean skipCacheStream = false;
     
     
     /**
      * Date format pattern used to parse HTTP date headers in RFC 1123 format.
      */
-    public static final String PATTERN_RFC1123 = "EEE, dd MMM yyyy HH:mm:ss zzz";    
+    //public static final String PATTERN_RFC1123 = "EEE, dd MMM yyyy HH:mm:ss zzz";
 
     public CacheFilters(HttpRequest originalRequest,
             ChannelHandlerContext ctx) {
@@ -82,35 +75,23 @@ public class CacheFilters implements HttpFilters {
         this.appDeck = AppDeck.getInstance();
     }
 
+    /*public CacheFilters(HttpRequest originalRequest, String answer) {
+        super(originalRequest, null);
+        //this.answer = answer;
+    }*/
+
     public CacheFilters(HttpRequest originalRequest) {
         this(originalRequest, null);
     }
 
     private HttpResponse forceCache(HttpResponse response)
     {
-//		response.headers().set("Pragma", "public");
     	String eTag = response.headers().get("ETag");
     	if (eTag == null)
     		response.headers().set("ETag", "appdeck-"+ Utils.randInt(0, 2000000));	
 		response.headers().set("Cache-Control", "public, max-age=2592000, max-stale=4592000");
-		/*
-		response.headers().set("Date", DateUtils.formatDate(now));
-		response.headers().set("Last-modified", DateUtils.formatDate(now));
-		response.headers().set("ETag", "\""+ cacheName + "-appdeck-"+ Utils.randInt(0, 2000000) + "\"");
-		response.headers().set("Expires", DateUtils.formatDate(expire));    	
-    	*/
 		return response;
     }
-    /*
-    private HttpResponse storeInCache(HttpResponse response, int maxage)
-    {
-    	return setCache(response, "store", maxage);
-    }    
-    
-    private HttpResponse backupInCache(HttpResponse response)
-    {
-    	return setCache(response, "backup", Integer.MAX_VALUE);    	
-    }    */ 
     
     private HttpResponse createCachedHTTPResponse(CacheManagerCachedResponse cachedResponse)
     {
@@ -125,42 +106,30 @@ public class CacheFilters implements HttpFilters {
                     HttpVersion.HTTP_1_1,
                     HttpResponseStatus.OK,
                     buf);
-    		
-    		//List<Map.Entry<String,String>> headers
-    		
+
+            HttpHeaders httpHeaders = response.headers();
+
     	    @SuppressWarnings("unchecked")
 			Iterator<String> keys = headers.keys();
     	    while (keys.hasNext())
     	    {
-    	        String key = keys.next();
-    	        String val = null;
     	        try {
-    	        	val = headers.getString(key);
-    	        	response.headers().set(key, val);
+                    String key = keys.next();
+    	        	String val = headers.getString(key);
+                    Log.d(TAG, "setHeader: "+key+": "+val);
+                    httpHeaders.set(key, val);
     	        } catch(Exception e){
     	        	e.printStackTrace();
     	        }
     	    }
-    		
-/*    		if (contentType == null)
-    			contentType = "application/octet-stream";
-    		response.headers().set("Content-Type", contentType);
 
-    		response.headers().set("Content-Length", data.length);
-				    			
-			response.headers().set("Vary", "Accept-Encoding");
-			response.headers().set("Server", "AppDeck-Embed-Files/1.0");
-			response.headers().set("X-Cache", "HIT");
-			response.headers().set("Accept-Ranges", "bytes");*/
-							
+            httpHeaders.set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
+
 			forceCache(response);
-			
-			//response.headers().set("Via", "1.1.10.0.2.15");
     		
     		return response;
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
@@ -168,6 +137,7 @@ public class CacheFilters implements HttpFilters {
     
     @Override
     public HttpResponse clientToProxyRequest(HttpObject httpObject) {
+
 /*
         if (httpObject instanceof HttpRequest) {
 
@@ -191,10 +161,10 @@ public class CacheFilters implements HttpFilters {
     		//DefaultHttpRequest request = (DefaultHttpRequest)httpObject;
             HttpRequest request = (HttpRequest) httpObject;
     		
-    		originalMethod = request.getMethod();
+    		originalMethod = request.method();
 
-    		absoluteURL = request.getUri();
-    		if (absoluteURL.startsWith("http://") == false && absoluteURL.startsWith("https://") == false)
+    		absoluteURL = request.uri();
+    		if (!absoluteURL.startsWith("http://") && !absoluteURL.startsWith("https://"))
     		{
 	    		if (absoluteURL.endsWith(":443"))
 	    			absoluteURL = "https://" + absoluteURL.substring(0, absoluteURL.length() - 4);
@@ -215,16 +185,11 @@ public class CacheFilters implements HttpFilters {
     		// change user agent
     		String ua = request.headers().get("User-Agent");
             if (ua != null)
-        		forceReadFromCache = ua.indexOf("FORCE_CACHE") != -1;
+        		forceReadFromCache = ua.contains("FORCE_CACHE");
             else
             	Log.e(TAG, "request without User-Agent: "+absoluteURL);
-    		/*if (ua != null)
-    		{
-    			ua = ua + " AppDeck"+(appDeck.isTablet? "-tablet" : "-phone" )+" "+appDeck.packageName+"/"+appDeck.config.app_version;
-    			request.headers().set("User-Agent", ua);
-    		}*/
 
-            request.headers().set("User-Agent", appDeck.userAgent);
+            //request.headers().set("User-Agent", appDeck.userAgent);
 
     		forceCache = appDeck.cache.shouldCache(absoluteURL);
 
@@ -236,7 +201,7 @@ public class CacheFilters implements HttpFilters {
 			boolean isCacheMiss = false;
 
     		// request should be cached ?
-            if (disableCache == false && (forceCache || forceReadFromCache))
+            if (!disableCache && (forceCache || forceReadFromCache))
     		{
         		// Already in browser cache ?
         		String etag = request.headers().get("If-None-Match");
@@ -263,10 +228,10 @@ public class CacheFilters implements HttpFilters {
         		}
 				isCacheMiss = true;
     		}
-    		    		
+
     		// embed file ?
     		CacheManagerCachedResponse embedResponse = appDeck.cache.getEmbedResponse(absoluteURL);
-    		if (disableCache == false && embedResponse != null)
+    		if (!disableCache && embedResponse != null)
     		{
         		// already in browser cache ?
         		String etag = request.headers().get("If-None-Match");
@@ -280,7 +245,7 @@ public class CacheFilters implements HttpFilters {
     	    		return response;
         		}
     			// create a embed cached response
-        		HttpResponse response = createCachedHTTPResponse(embedResponse);       			
+        		HttpResponse response = createCachedHTTPResponse(embedResponse);
        			if (response != null)
        			{	    			
 	    			Log.i(TAG, "EMBED: "+absoluteURL);//+" Size:"+(data.length/1024)+"Kb");
@@ -313,7 +278,6 @@ public class CacheFilters implements HttpFilters {
 
     	}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -344,12 +308,13 @@ public class CacheFilters implements HttpFilters {
     
     @Override
     public HttpObject serverToProxyResponse(HttpObject httpObject) {
+
     	try {
     	// create output stream that will store cache
-		if (skipCacheStream == false && cacheStream == null && httpObject instanceof HttpResponse)
+		if (!skipCacheStream && cacheStream == null && httpObject instanceof HttpResponse)
 		{
 			HttpResponse response = (HttpResponse)httpObject;
-			HttpResponseStatus status = response.getStatus();
+			HttpResponseStatus status = response.status();
 			
 			int code = status.code();
 			String reason = status.reasonPhrase();
@@ -389,7 +354,7 @@ public class CacheFilters implements HttpFilters {
 		}
 		
     	// make sure cachable content get right cache headers
-		if (forceCache == true && httpObject instanceof HttpResponse)
+		if (!forceCache && httpObject instanceof HttpResponse)
 		{
 			Log.i(TAG, "< CACHE MISS SEND: " + absoluteURL);
 			DebugLog.info("CACHE MISS", absoluteURL);
@@ -444,11 +409,11 @@ public class CacheFilters implements HttpFilters {
 
 	@Override
     public HttpObject proxyToClientResponse(HttpObject httpObject) {
-    	
+
 		if (httpObject instanceof HttpResponse)
 		{
 			HttpResponse response = (HttpResponse)httpObject;
-			HttpResponseStatus status = response.getStatus();
+			HttpResponseStatus status = response.status();
 			
 			int code = status.code();
 			String reason = status.reasonPhrase();
