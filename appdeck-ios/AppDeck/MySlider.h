40619cd0a141db191f532560693f4d4475296c48
hanine1 <hanine.bensaad90@gmail.com>
2018-04-26 15:57:53 +0100

    commit after slider creation

---
+++ appdeck-ios/AppDeck/MySlider.h
@@ -0,0 +1,13 @@
+//
+//  MySlider.h
+//  AppDeck
+//
+//  Created by hanine ben saad on 24/04/2018.
+//  Copyright © 2018 Mathieu De Kermadec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MySlider : UISlider
+
+@end
---
+++ appdeck-ios/AppDeck/MySlider.m
@@ -0,0 +1,21 @@
+//
+//  MySlider.m
+//  AppDeck
+//
+//  Created by hanine ben saad on 24/04/2018.
+//  Copyright © 2018 Mathieu De Kermadec. All rights reserved.
+//
+
+#import "MySlider.h"
+
+@implementation MySlider
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
